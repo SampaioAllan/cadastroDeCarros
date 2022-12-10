0cd4becc0f3edd7dff5e2efb0851a2e2ce676a36
Allan Sampaio <lanbrendo93@gmail.com>
2022-12-10 14:08:20 -0300

    Criei a função que adiciona um carro a lista e depois salva no localStorage e função que ao atualizar a página é logado a lista salva no localStorage

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-const formulario = document.getElementById("formulario");
-const marca = document.getElementById("marca");
-const modelo = document.getElementById("modelo");
-const ano = document.getElementById("ano");
-const valor = document.getElementById("valor");
-const cor = document.getElementById("cor");
-const cambio = document.getElementById("cambio");
+let carros = [
+    {
+        marca: "fiat",
+        modelo: "uno",
+        ano: 2010,
+        valor: 20000.00,
+        cor: "verde",
+        cambioAutomatico: true
+    },
+    {
+        marca: "fiat",
+        modelo: "500",
+        ano: 2018,
+        valor: 387465.00,
+        cor: "verde",
+        cambioAutomatico: true
+    }
+]
 
-function GerarMsgDeEspera(e){
-    console.log(marca.value);
-    console.log(modelo.value);
-    console.log(ano.value);
-    console.log(valor.value);
-    console.log(cor.value);
-    console.log(cambio.value);
-    e.preventDefault();
+const formulario = document.getElementById("formulario");
+
+function addCarro(e) {
+    e.preventDefault(); 
+    let carro = {
+        marca: document.getElementById("marca").value,
+        modelo: document.getElementById("modelo").value,
+        ano: document.getElementById("ano").value,
+        valor: document.getElementById("valor").value,
+        cor: document.getElementById("cor").value,
+        cambioAutomatico: document.getElementById("cambioAutomatico").value
+    }
+    carros.push(carro)
+    localStorage.setItem("JSONCarros", JSON.stringify(carros));
 }
 
-formulario.addEventListener('submit', GerarMsgDeEspera);
+function logarcarrosdoLocalStorage() {
+    carros = JSON.parse(localStorage.getItem('JSONCarros'));
+    console.log(carros);
+}
+
+formulario.addEventListener('submit', addCarro);
+window.addEventListener('load', logarcarrosdoLocalStorage);
